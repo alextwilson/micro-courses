7c8dc2571e45860d04894b7d23bceb3b0978aace
alextwilson <github@alextwilson.co.uk>
2018-07-27 16:27:38 +0100

    Changes header to material design typography header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Micro Courses</h1>
+        <Typography variant='display1' align='center' gutterBottom>
+          Micro Courses
+        </Typography>
       </div>
     );
   }
