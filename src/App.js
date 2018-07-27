baa83317e45c93062263d6e138b76472958553b4
alextwilson <github@alextwilson.co.uk>
2018-07-27 17:02:09 +0100

    Adds small course card as a component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography';
+import CourseCard from './components/CourseCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Typography variant='display1' align='center' gutterBottom>
+        <Typography variant='display2' align='center' gutterBottom>
           Micro Courses
         </Typography>
+        <CourseCard />
       </div>
     );
   }
