b1ecafea3702cd216becca70b3633b5a5faaf958
alextwilson <github@alextwilson.co.uk>
2018-08-01 17:25:54 +0100

    Refactors CourseCard component to function
    This fits the recommended react style of using functions instead
    of classes when a component is stateless.

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 
-class CourseCard extends Component {
-  render() {
-    return (
-      <div className="CourseCard">
-        <Card>
-          <Typography variant='display1' align='center'>
-            Micro Course
-          </Typography>
-        </Card>
-      </div>
-    );
-  }
+function CourseCard() {
+  return (
+    <div className="CourseCard">
+      <Card>
+        <Typography variant='display1' align='center'>
+          Micro Course
+        </Typography>
+      </Card>
+    </div>
+  );
 }
 
 export default CourseCard;
