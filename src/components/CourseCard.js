a8585496a0f9b9568bfb5590b9cbf5aa2ab898ff
alextwilson <github@alextwilson.co.uk>
2018-08-01 17:30:24 +0100

    Wraps card content inside CardContent component

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 function CourseCard() {
   return (
-    <div className="CourseCard">
+    <div>
       <Card>
-        <Typography variant='display1' align='center'>
-          Micro Course
-        </Typography>
+        <CardContent>
+          <Typography variant='display1' align='center'>
+            Micro Course Content
+          </Typography>
+        </CardContent>
       </Card>
     </div>
   );
