c1972798f8fe48b99b40663d200fcb54a15e3c68
alextwilson <github@alextwilson.co.uk>
2018-08-07 20:07:54 +0100

    Generates CourseCard from content array

--- src/components/CoursesContainer.js
+++ src/components/CoursesContainer.js
@@ -3,18 +3,18 @@ import Grid from '@material-ui/core/Grid';
 import CourseCard from './CourseCard';
 
 function CoursesContainer() {
+  const content = [{image: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Robinson-5400.jpg',
+                    title: 'Geopolitics',
+                    text: 'Geopolitics content'}];
+
   return (
     <div>
       <Grid container spacing={24} justify="center">
-        <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
-          <CourseCard image='https://upload.wikimedia.org/wikipedia/commons/8/84/Robinson-5400.jpg' title='Geopolitics' text='Geopolitics content'/>
-        </Grid>
-        <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
-          <CourseCard text='Course 2 content'/>
-        </Grid>
-        <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
-          <CourseCard text='Course 3 content'/>
-        </Grid>
+        {content.map(course => (
+          <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}} key={ course.title }>
+            <CourseCard image={ course.image } title={ course.title } text={ course.text }/>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
