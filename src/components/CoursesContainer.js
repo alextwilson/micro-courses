a77d1f03d18d0bb36b7510057d62b55450d87c97
alextwilson <github@alextwilson.co.uk>
2018-08-07 19:35:07 +0100

    Uses open source image

--- src/components/CoursesContainer.js
+++ src/components/CoursesContainer.js
@@ -7,7 +7,7 @@ function CoursesContainer() {
     <div>
       <Grid container spacing={24} justify="center">
         <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
-          <CourseCard image='https://cdn.shopify.com/s/files/1/0977/4104/products/MC-EUR-955566.jpeg?v=1530306094' title='Geopolitics' text='Geopolitics content'/>
+          <CourseCard image='https://upload.wikimedia.org/wikipedia/commons/8/84/Robinson-5400.jpg' title='Geopolitics' text='Geopolitics content'/>
         </Grid>
         <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
           <CourseCard text='Course 2 content'/>
