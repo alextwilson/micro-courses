d0e32dd4597f8806080c40e014bb392f1a4f58e7
alextwilson <github@alextwilson.co.uk>
2018-08-07 13:48:35 +0100

    CourseCards centered in grid

--- src/components/CoursesContainer.js
+++ src/components/CoursesContainer.js
@@ -5,7 +5,7 @@ import CourseCard from './CourseCard';
 function CoursesContainer() {
   return (
     <div>
-      <Grid container spacing={24}>
+      <Grid container spacing={24} justify="center">
         <Grid item lg={4} xl={3}>
           <CourseCard text='Course 1 content'/>
         </Grid>
