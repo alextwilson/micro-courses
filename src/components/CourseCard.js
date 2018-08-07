e8afaf05b30279582462102b5f29c2182b21108f
alextwilson <github@alextwilson.co.uk>
2018-08-07 19:20:12 +0100

    Adds image to card via props
    Cards have max width

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -9,8 +10,15 @@ function CourseCard(props) {
   return (
     <div>
       <Card>
+        <CardMedia
+          style={{height: 0, paddingTop: '56.25%'}}
+          image={ props.image }
+        />
         <CardContent>
-          <Typography variant='display1' align='center'>
+          <Typography gutterBottom variant="headline" component="h2">
+            { props.title }
+          </Typography>
+          <Typography component="p">
             { props.text }
           </Typography>
         </CardContent>
--- src/components/CoursesContainer.js
+++ src/components/CoursesContainer.js
@@ -6,13 +6,13 @@ function CoursesContainer() {
   return (
     <div>
       <Grid container spacing={24} justify="center">
-        <Grid item lg={4} xl={3}>
-          <CourseCard text='Course 1 content'/>
+        <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
+          <CourseCard image='https://cdn.shopify.com/s/files/1/0977/4104/products/MC-EUR-955566.jpeg?v=1530306094' title='Geopolitics' text='Geopolitics content'/>
         </Grid>
-        <Grid item lg={4} xl={3}>
+        <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
           <CourseCard text='Course 2 content'/>
         </Grid>
-        <Grid item lg={4} xl={3}>
+        <Grid item xs={12} sm={6} lg={4} xl={3} style={{maxWidth: 345}}>
           <CourseCard text='Course 3 content'/>
         </Grid>
       </Grid>
