e03f87e7ffe216c77b225deaddecb6822fe5e45a
alextwilson <github@alextwilson.co.uk>
2018-10-17 23:48:36 +0100

    Changes course resources to array instead of individual items

--- src/components/CoursesContainer.js
+++ src/components/CoursesContainer.js
@@ -8,19 +8,21 @@ function CoursesContainer() {
       image: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Robinson-5400.jpg',
       title: 'Geopolitics',
       text: 'Geopolitics is the study of the effects of geography (human and physical) on politics and international relations.',
-      resource1: {
-        image: 'https://images.penguinrandomhouse.com/cover/9780812984804',
-        description: "Asia's Cauldron: The South China Sea and the End of a Stable Pacific by Robert D. Kaplan is an excellent introduction to geopolitics. It is well written and concise, deals with contemporary subject matter, and focuses on a (relatively) small geographical area."
-      },
-      resource2: {
-        image: 'https://upload.wikimedia.org/wikipedia/en/f/f6/The_Next_100_Years.jpg',
-        description: "The Next 100 Years: A Forecast for the 21st Century is a book by George Friedman has a much wider scope, both geographically and chronologically. What makes it a particularly interesting study is seeing which of George Friedman's predictions have, and more importantly have not come to pass in the 9 years since this book's publication."
-      },
-      resource3: {
-        id: 3,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/9/90/The_Influence_of_Sea_Power_Upon_History.jpg',
-        description: "The Influence of Sea Power Upon History: 1660–1783 by Alfred Thayer Mahan is widely considered the foundational work of geopolitics. Written in the late 19th century whilst Mahan was president of the US Naval War College, the book examines the usage of naval power by European and American navies, particularly how Great Britain's control of the sea allowed the growth and maintenance of the world's largest empire."
-      }
+      resources: [
+        {
+          id: 1,
+          image: 'https://images.penguinrandomhouse.com/cover/9780812984804',
+          description: "Asia's Cauldron: The South China Sea and the End of a Stable Pacific by Robert D. Kaplan is an excellent introduction to geopolitics. It is well written and concise, deals with contemporary subject matter, and focuses on a (relatively) small geographical area."
+        }, {
+          id: 2,
+          image: 'https://upload.wikimedia.org/wikipedia/en/f/f6/The_Next_100_Years.jpg',
+          description: "The Next 100 Years: A Forecast for the 21st Century is a book by George Friedman has a much wider scope, both geographically and chronologically. What makes it a particularly interesting study is seeing which of George Friedman's predictions have, and more importantly have not come to pass in the 9 years since this book's publication."
+        }, {
+          id: 3,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/9/90/The_Influence_of_Sea_Power_Upon_History.jpg',
+          description: "The Influence of Sea Power Upon History: 1660–1783 by Alfred Thayer Mahan is widely considered the foundational work of geopolitics. Written in the late 19th century whilst Mahan was president of the US Naval War College, the book examines the usage of naval power by European and American navies, particularly how Great Britain's control of the sea allowed the growth and maintenance of the world's largest empire."
+        }
+      ]
     }
   ];
 
@@ -33,9 +35,7 @@ function CoursesContainer() {
               image={ course.image }
               title={ course.title }
               text={ course.text }
-              resource1={ course.resource1 }
-              resource2={ course.resource2 }
-              resource3={ course.resource3 }
+              resources={ course.resources }
             />
           </Grid>
         ))}
