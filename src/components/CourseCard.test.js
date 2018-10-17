3a20c5b89bab512af71c841f81f3495d116c3bdb
alextwilson <github@alextwilson.co.uk>
2018-10-17 23:46:48 +0100

    Adds props to CourseCard test

--- src/components/CourseCard.test.js
+++ src/components/CourseCard.test.js
@@ -4,6 +4,6 @@ import CourseCard from './CourseCard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<CourseCard />, div);
+  ReactDOM.render(<CourseCard image={'foo'} title={'bar'} text={'baz'} resources= {['foo', 'bar']}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
